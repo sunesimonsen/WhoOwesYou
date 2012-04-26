bbb1e6a291d0d61a677d847ec8017433b290748b
Sune Simonsen <sune.simonsen@jayway.com>
2012-04-26 18:21:50 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhoOwesYou::Application.config.secret_token = '269e7934cd4f129005f61addb8b440c88dde09b92dd636dc72b858481ac3be1a6c70e825001806e3d746d303f83566ec672ae2c67d243e7f8b21b4167f81351e'
