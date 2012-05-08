eefead4080bfc8bf8a5688f11edbdb37acee53bb
Sune Simonsen <sune.simonsen@jayway.com>
2012-05-08 10:56:54 +0200

    require spec_helper without absolute path

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,7 +1,6 @@
-require File.dirname(__FILE__)+'/../spec_helper'
+require 'spec_helper' 
 
 describe Participant do
-
 
   it "requires a user and an arragement to be valid" do
     subject.should_not be_valid
