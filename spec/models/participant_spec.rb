2fe137dd76757ee86a05bc4a6e9a8f2065a9a5e2
Sune Simonsen <sune.simonsen@jayway.com>
2012-05-09 13:52:49 +0200

    more tests

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper' 
 
 describe Participant do
+  fixtures :users, :arrangements
 
   it "requires a user and an arragement to be valid" do
     subject.should_not be_valid
-    subject.user = User.create :name => "User"
-    subject.arrangement = Arrangement.create :name => "Arrangement"
+    subject.user = users :john
+    subject.arrangement = arrangements :party
     subject.should be_valid
   end 
 
@@ -13,6 +14,12 @@ describe Participant do
     subject.expenses << Expense.new(:name => "Beer", :amount => 250)
     subject.expenses << Expense.new(:name => "Chips", :amount => 100)
     subject.total_amount.should eql(350)
+  end
+
+  it "have the same name as its user" do
+    subject.user = users :john
+    subject.user.name = "user name"
+    subject.name.should eql("user name")
   end
 
 end 
