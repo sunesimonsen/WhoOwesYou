96f20e91a5dddd9cbbe716c95a233fbd9e43dc0a
Sune Simonsen <sune.simonsen@jayway.com>
2012-05-15 16:35:00 +0200

    implemented added expenses to a participant

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,4 +8,10 @@ class ExpensesController < ApplicationController
     redirect_to arrangement_participant_path(arrangement, participant)
   end
 
+  def create
+    arrangement = Arrangement.find(params[:arrangement_id])
+    participant = arrangement.participants.find(params[:participant_id])
+    participant.expenses.build(:name => params[:name], :amount => params[:amount]).save
+    redirect_to arrangement_participant_path(arrangement, participant)
+  end 
 end
