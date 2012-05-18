9dd6be58dae397467e5e2bef8a679b28924491eb
Sune Simonsen <sune.simonsen@jayway.com>
2012-05-18 23:57:51 +0200

    refactoring tests

--- app/models/creditor.rb
+++ app/models/creditor.rb
@@ -11,6 +11,8 @@ class Creditor
     transfer_amount = debitor.debt < claim ? debitor.debt : claim
     @claim = @claim - transfer_amount
     debitor.debt = debitor.debt - transfer_amount
-    Transfer.new :from => debitor.participant, :to => @participant, :amount => transfer_amount 
+    Transfer.new :from => debitor.participant,
+                 :to => @participant,
+                 :amount => transfer_amount 
   end 
 end 
