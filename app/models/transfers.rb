5dd8e04f2483c1b7b1c84b44ccdea4244a0c27b9
Sune Simonsen <sune.simonsen@jayway.com>
2012-05-29 19:39:39 +0200

    refactoring

--- app/models/transfers.rb
+++ app/models/transfers.rb
@@ -6,16 +6,12 @@ class Transfers < Set
   end
     
   def from participant
-    participant_transfers = find_all do |t|
-      t.from == participant
-    end 
+    participant_transfers = find_all { |t| t.from == participant } 
     Transfers.new participant_transfers
   end
 
   def to participant
-    participant_transfers = find_all do |t|
-      t.to == participant
-    end 
+    participant_transfers = find_all { |t| t.to == participant } 
     Transfers.new participant_transfers
   end
 end 
